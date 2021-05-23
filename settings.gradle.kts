712d87025f4654664775e42fb8704a8c07997468
Jesus Zazueta <jjzazuet@vacco.io>
2021-05-23 01:09:00 -0400

    - Rolling back multi module structure.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
 pluginManagement { repositories { mavenCentral(); gradlePluginPortal() } }
 
-include("rv-gradle-plugin", "rv-kit-undertow")
-
-project(":rv-gradle-plugin").name = "io.vacco.ronove.gradle.plugin"
+rootProject.name = "io.vacco.ronove.gradle.plugin"
