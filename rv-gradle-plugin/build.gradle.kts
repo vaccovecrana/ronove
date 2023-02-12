0cf271e9e12c67113d6baaf6501bea5db0188c3a
jjzazuet <jjzazuet@vacco.io>
2023-02-12 04:27:01 -0500

    - Initial release.

--- rv-gradle-plugin/build.gradle.kts
+++ rv-gradle-plugin/build.gradle.kts
@@ -6,8 +6,3 @@ dependencies {
   implementation("cz.habarta.typescript-generator:typescript-generator-gradle-plugin:3.1.1185")
   implementation("io.marioslab.basis:template:1.7")
 }
-
-configurations.all {
-  exclude("javax.xml.bind", "jaxb-api")
-  exclude("javax.activation", "javax.activation-api")
-}
