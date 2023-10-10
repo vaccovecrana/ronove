f52dc894f50959e899cbfc54e2cf8a756a0717a6
Jesus Zazueta <jjzazuet@gmail.com>
2023-10-10 01:25:11 -0400

    - rv-kit dependency updates.

--- rv-gradle-plugin/src/main/java/io/vacco/ronove/plugin/RvPlugin.java
+++ rv-gradle-plugin/src/main/java/io/vacco/ronove/plugin/RvPlugin.java
@@ -8,7 +8,7 @@ public class RvPlugin implements Plugin<Project> {
   public static final String implementation = "implementation";
 
   @Override public void apply(Project project) {
-    project.getDependencies().add(implementation, "jakarta.ws.rs:jakarta.ws.rs-api:3.0.0");
+    project.getDependencies().add(implementation, "jakarta.ws.rs:jakarta.ws.rs-api:3.1.0");
     project.getExtensions().create("ronove", RvPluginExtension.class, project);
 
     var classes = project.getTasks().getByName("classes");
