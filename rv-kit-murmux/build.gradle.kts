5c9a51482498be7e042e8fc9d16d179887e648ee
Jesus Zazueta <jjzazuet@gmail.com>
2025-05-26 08:21:16 -0400

    - Dependency upgrades. - Minor version bump.

--- rv-kit-murmux/build.gradle.kts
+++ rv-kit-murmux/build.gradle.kts
@@ -1,12 +1,8 @@
 configure<io.vacco.oss.gitflow.GsPluginProfileExtension> { sharedLibrary(true, false) }
 
 val api by configurations
-val murmuxVer = "[2,)"
-val mxp = project(":rv-core")
-
-version = "${mxp.version}_${murmuxVer}"
 
 dependencies {
   api(project(":rv-core"))
-  api("io.vacco.murmux:murmux:${murmuxVer}")
+  api("io.vacco.murmux:murmux:[2,)")
 }
